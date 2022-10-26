8a3c2af490d91b4fd2cd04a869267e7b020389f7
apple <bronzels@hotmail.com>
2022-10-26 14:13:04 +0800

    1，k8s-sealos集群功能，dns性能，镜像和harbor私仓测试。

--- harbor/setup.sh
+++ harbor/setup.sh
@@ -1,5 +1,6 @@
 #https://github.com/goharbor/harbor/releases/tag/v2.1.2
-tar xzvf harbor-offline-installer-v2.1.2.tgz
+rev=2.6.1
+tar xzvf harbor-offline-installer-v$rev.tgz
 cd harbor
 cp harbor.yml.tmpl harbor.yml
 vim harbor.yml
@@ -10,3 +11,5 @@ vim harbor.yml
 docker login harbor.my.org:1080
 #admin密码Harbor12345
 
+#sudo lsof -nP -p 32239 | grep LISTEN
+sudo lsof -nP | grep LISTEN
