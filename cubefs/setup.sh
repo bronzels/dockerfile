3e608c832799e59689722e60993e3c36c1702df8
apple <bronzels@hotmail.com>
2022-10-28 10:15:06 +0800

    1，在sealos安装测试以后，重新安装配置cubefs。2，成功安装，还未测试功能和性能。

--- cubefs/setup.sh
+++ cubefs/setup.sh
@@ -1,43 +1,6 @@
 git clone https://github.com/cubefs/cubefs-helm
-cd cubefs-helm/
-cp ~/.kube/config chubaofs/config/kubeconfig
-
-file=./chubaofs.yaml
-cat << \EOF > $file
-component:
-  provisioner: true
-  monitor: true
-  ingress: false
-  objectnode: false
-
-path:
-  data: /chubaofs/data
-  log: /chubaofs/log
-
-master:
-  replicas: 3
-
-datanode:
-  disks:
-    - /data0:429496729600
-
-metanode:
-  resources:
-    requests:
-      memory: "3Gi"
-      cpu: "1000m"
-    limits:
-      memory: "6Gi"
-      cpu: "2000m"
-  total_mem: "26843545600"
-
-provisioner:
-  kubelet_path: /var/lib/kubelet
-
-storageClass:
-  default: true
-
-EOF
+cd cubefs-helm/chubaofs
+cp ~/.kube/config config/kubeconfig
 
 kubectl label node dtpct chubaofs-master=enabled
 kubectl label node mdlapubu chubaofs-master=enabled
@@ -71,7 +34,8 @@ mount|grep data0
 #/dev/disk/by-uuid/0f57c0db-9adc-4ae9-8348-3bba4d5579eb /data0 xfs defaults 0 1
 
 kubectl create ns chubaofs
-helm install chubaofs ./chubaofs -f ./chubaofs.yaml -n chubaofs
+cp ../../values.yaml ./
+helm install chubaofs ./ -n chubaofs
 #helm uninstall chubaofs -n chubaofs
 
 k get pod -n chubaofs
