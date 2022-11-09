10d2e9976a5933132757abf03eafe2352b9be9ff
apple <bronzels@hotmail.com>
2022-11-09 16:30:59 +0800

    1，部署hdfs到k8s，测试DFSio，小文件性能在65m接近本地文件测试80m。2，部署hive，测试创建和查询表通过。3，部署minio，console测试本地put/delete通过。(mc mac客户端下载太耗时，linux客户端下载404，还未完成）

--- cubefs/hadoop.sh
+++ cubefs/hadoop.sh
@@ -2,11 +2,10 @@ git clone https://github.com/cubefs/cubefs-hadoop.git
 #in idea
 mvn package -Dmaven.test.skip=true
 
-docker run --name centosdev -d --network=host -v /Volumes/data/workspace:/root/workspace harbor.my.org:1080/base/python:3.8-centos7-netutil-ccplus7-go tail -f /dev/null
 #ssh remote dev
 docker run --privileged --name centosdev -d -p 1022:22 -v /Volumes/data/workspace:/root/workspace harbor.my.org:1080/base/python:3.8-centos7-netutil-ccplus7-go-mpich /usr/sbin/init
+docker exec -it centosdev /bin/bash
   systemctl start sshd
   systemctl status sshd
-
-docker exec -it centosdev /bin/bash
+ssh root@localhost -p 1022
 
