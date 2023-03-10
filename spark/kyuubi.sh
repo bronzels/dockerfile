1211f04f1c9df8bf2594f70f9ce62e88568391ce
apple <bronzels@hotmail.com>
2023-03-10 12:31:41 +0800

    1，starrocks能外表查询juicefs数据，性能比presto快3倍，比doris内表慢7倍。2，kyuubi设置成功，能直接在k8s上用spark-sql查询hive表了。连接模式设为CONNECTOR以后，资源会销毁，默认USER模式不会。在beeline连接时可以指定volcano podtemplategroup和driver/executor 资源等配置项会覆盖静态文件配置项，但是-i配置项发生在driver创建之前，不会生效。（executor也不会生效，这个有点疑问）

--- spark/kyuubi.sh
+++ spark/kyuubi.sh
@@ -29,31 +29,34 @@ BASE_IMAGE=harbor.my.org:1080/bronzels/spark-juicefs-volcano-rss:${SPARK_VERSION
 
 cd ${SPARK_HOME}
 
-:<<EOF
-wget -c https://github.com/apache/kyuubi/archive/refs/tags/v${KYUUBI_VERSION}.tar.gz
-tar xzvf kyuubi-${KYUUBI_VERSION}.tar.gz
-cd kyuubi-${KYUUBI_VERSION}/docker/
-EOF
-
 #wget -c https://archive.apache.org/dist/incubator/kyuubi/kyuubi-1.6.1-incubating/apache-kyuubi-1.6.1-incubating-bin.tgz
 wget -c https://dlcdn.apache.org/kyuubi/kyuubi-1.7.0/apache-kyuubi-1.7.0-bin.tgz
 
+
 tar xzvf apache-kyuubi-${KYUUBI_VERSION}-bin.tgz
-cd ${KYUUBI_HOME}/docker/
-file=Dockerfile
+cd ${KYUUBI_HOME}
+cp ${SPARK_HOME}/spark-defaults.conf ./
+$SED -i '/spark.kubernetes.scheduler.name volcano/,+2d' spark-defaults.conf
+$SED -i '/spark.kubernetes.driver.volumes.persistentVolumeClaim.juicefsvol.mount.path/,+5d' spark-defaults.conf
+
+file=docker/Dockerfile
 cp ${file} ${file}.bk
 #$SED -i 's/spark-binary/\/app\/hdfs\/spark/g' ${file}
 #$SED -i '/    rm -rf \/var\/cache\/apt/i\    mkdir ${KYUUBI_WORK_DIR_ROOT}/kyuubi && chmod a+rwx -R ${KYUUBI_WORK_DIR_ROOT}/kyuubi && \\' ${file}
 #$SED -i '/    rm -rf \/var\/cache\/apt/i\    mkdir ${KYUUBI_WORK_DIR_ROOT}/hdfs && chown hdfs:hdfs -R ${KYUUBI_WORK_DIR_ROOT}/hdfs && chmod a+rwx -R ${KYUUBI_WORK_DIR_ROOT}/hdfs && \\' ${file}
+
+$SED -i "/USER \${kyuubi_uid}/i\COPY --chown=hdfs:root spark-defaults.conf \${SPARK_HOME}/conf/spark-defaults.conf" ${file}
+#built-in
+:<<EOF
 $SED -i 's/spark-binary/spark/g' ${file}
 $SED -i '/COPY LICENSE NOTICE RELEASE/i\RUN chown -R kyuubi:root ${SPARK_HOME}' ${file}
-
-cd ${KYUUBI_HOME}
 
 kubectl cp -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep spark-test | awk '{print $1}'`:/app/hdfs/spark park
 chmod a+x spark/bin/*
 chmod a+x spark/sbin/*
 file=spark/conf/core-site.xml
+EOF
+#built-in
 $SED -i "s@</configuration>@@g" ${file}
 cat << EOF >> ${file}
     <property>
@@ -72,6 +75,7 @@ cat << EOF >> ${file}
 EOF
 
 DOCKER_BUILDKIT=1 docker build ./ -f docker/Dockerfile --progress=plain --build-arg spark_provided="spark_provided" --build-arg spark_home_in_docker="/app/hdfs/spark" --build-arg BASE_IMAGE="${BASE_IMAGE}" -t harbor.my.org:1080/sqlengine/kyuubi-juicefs-volcano-rss:${KYUUBI_VERSION}
+#built-in
 #DOCKER_BUILDKIT=1 docker build ./ -f docker/Dockerfile --progress=plain -t harbor.my.org:1080/sqlengine/kyuubi-juicefs-volcano-rss:${KYUUBI_VERSION}
 docker push harbor.my.org:1080/sqlengine/kyuubi-juicefs-volcano-rss:${KYUUBI_VERSION}
 
@@ -89,14 +93,16 @@ cd ${KYUUBI_HOME}/charts/kyuubi
 file=kyuubi-configmap.yaml
 cp templates/${file} ${file}.bk
 $SED -i "s@    kyuubi.frontend.bind.host=localhost@    kyuubi.frontend.bind.host=0.0.0.0@g" templates/${file}
-$SED -i "/    kyuubi.frontend.protocols/a\    spark.kubernetes.container.image=harbor.my.org:1080\/bronzels\/spark-juicefs-volcano-rss:3.3.1\n    spark.kubernetes.scheduler.volcano.podGroupTemplateFile=\/app\/hdfs\/spark\/work-dir\/podgroups\/volcano-halfavailable-podgroup.yaml\n    spark.kubernetes.scheduler.name=volcano\n    spark.kubernetes.driver.pod.featureSteps=org.apache.spark.deploy.k8s.features.VolcanoFeatureStep\n    spark.kubernetes.executor.pod.featureSteps=org.apache.spark.deploy.k8s.features.VolcanoFeatureStep" templates/${file}
+$SED -i "/    kyuubi.frontend.protocols/a\    spark.kubernetes.container.image=harbor.my.org:1080\/bronzels\/spark-juicefs-volcano-rss:3.3.1\n    spark.kubernetes.scheduler.volcano.podGroupTemplateFile=\/app\/hdfs\/spark\/work-dir\/podgroups\/volcano-halfavailable-podgroup.yaml\n    spark.kubernetes.scheduler.name=volcano\n    spark.kubernetes.driver.pod.featureSteps=org.apache.spark.deploy.k8s.features.VolcanoFeatureStep\n    spark.kubernetes.executor.pod.featureSteps=org.apache.spark.deploy.k8s.features.VolcanoFeatureStep\n    spark.submit.deployMode=cluster\n    kyuubi.ha.zookeeper.quorum=zk-zookeeper\n    kyuubi.engine.share.level=CONNECTION" templates/${file}
 
 file=values.yaml
 cp ${file} ${file}.bk
 $SED -i '/    - apiGroups: \[""\]/,+3d' ${file}
 $SED -i "/  rules:/a\  - apiGroups: [\"\"]\n    resources: [\"endpoints\"]\n    verbs: [\"get\", \"watch\", \"list\"]\n  - apiGroups: [\"\"]\n    resources: [\"pods\"]\n    verbs: [\"create\", \"delete\", \"update\", \"get\", \"watch\", \"list\"]\n  - apiGroups: [\"\"]\n    resources: [\"configmaps\"]\n    verbs: [\"create\", \"delete\", \"update\", \"get\", \"watch\", \"list\"]\n  - apiGroups: [\"\"]\n    resources: [\"services\"]\n    verbs: [\"create\", \"delete\", \"update\", \"get\", \"watch\", \"list\"]\n  - apiGroups: [\"\"]\n    resources: [\"persistentvolumeclaims\"]\n    verbs: [\"create\", \"delete\", \"update\", \"get\", \"watch\", \"list\"]\n  - apiGroups: [\"scheduling.volcano.sh\"]\n    resources: [\"podgroups\"]\n    verbs: [\"create\", \"delete\", \"update\", \"get\", \"watch\", \"list\"]\n" ${file}
-$SED -i "s/volumes: \[\]/volumes:\n  - name: juicefsvol\n    persistentVolumeClaim:\n      claimName: rss-juicefs-pvc/g" ${file}
-$SED -i "s/volumeMounts: \[\]/volumeMounts:\n  - mountPath: \/tmp\/sparklogs\n    name: juicefsvol/g" ${file}
+#后来确认只能在spark-defaults里定义，这里定义了没用
+#$SED -i "s/volumes: \[\]/volumes:\n  - name: juicefsvol\n    persistentVolumeClaim:\n      claimName: rss-juicefs-pvc/g" ${file}
+#$SED -i "s/volumeMounts: \[\]/volumeMounts:\n  - mountPath: \/tmp\/sparklogs\n    name: juicefsvol/g" ${file}
+#加到这里转换到kyuubi-defaults.conf里没有回车，只好直接修改configmap里的kyuubi-defaults.conf部分
 #$SED -i "s/  kyuubiDefaults: ~/  kyuubiDefaults:\n    spark.kubernetes.container.image=harbor.my.org:1080\/bronzels\/spark-juicefs-volcano-rss:3.3.1\n    spark.kubernetes.scheduler.volcano.podGroupTemplateFile=\/app\/hdfs\/spark\/work-dir\/podgroups\/volcano-halfavailable-podgroup.yaml\n    spark.kubernetes.scheduler.name=volcano\n    spark.kubernetes.driver.pod.featureSteps=org.apache.spark.deploy.k8s.features.VolcanoFeatureStep\n    spark.kubernetes.executor.pod.featureSteps=org.apache.spark.deploy.k8s.features.VolcanoFeatureStep/g" ${file}
 
 helm install zk bitnami/zookeeper -n spark-operator \
@@ -138,7 +144,7 @@ To connect to your ZooKeeper server from outside the cluster execute the followi
     zkCli.sh 127.0.0.1:2181
 EOF
 
-helm install kyuubi -n spark-operator -f values.yaml \
+helm install kyuubisrv -n spark-operator -f values.yaml \
   --set image.repository=harbor.my.org:1080/sqlengine/kyuubi-juicefs-volcano-rss \
   --set image.tag=${KYUUBI_VERSION} \
   --set replicaCount=1 \
@@ -168,15 +174,20 @@ THRIFT_BINARY:
     kubectl port-forward svc/my-thrift-binary 10009:10009 -n spark-operator
   and use 127.0.0.1:10009
 EOF
+
 watch kubectl get all -n spark-operator
 kubectl get all -n spark-operator
 
-helm uninstall kyuubi -n spark-operator
-kubectl get pod -n spark-operator |grep -v Running | grep kyuubi | awk '{print $1}'| xargs kubectl delete pod "$1" -n spark-operator --force --grace-period=0
-kubectl get pod -n spark-operator |grep Running | grep kyuubi | awk '{print $1}'| xargs kubectl delete pod "$1" -n spark-operator --force --grace-period=0
+helm uninstall kyuubisrv -n spark-operator
+kubectl get pod -n spark-operator |grep -v Running | grep kyuubisrv | awk '{print $1}'| xargs kubectl delete pod "$1" -n spark-operator --force --grace-period=0
 
-#kubectl logs -n spark-operator kyuubi-server-
-kubectl logs -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubi | awk '{print $1}'`
+kubectl get pod -n spark-operator |grep -v Running | grep kyuubi | awk '{print $1}'| xargs kubectl delete pod "$1" -n spark-operator --force --grace-period=0
+
+kubectl logs -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubisrv | awk '{print $1}'`
+kubectl logs -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubi-connection-spark-sql | awk '{print $1}'`
+kubectl exec -it -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubisrv | awk '{print $1}'` -- tail -f /opt/kyuubi/work/hdfs/kyuubi-spark-sql-engine.log.0
+kubectl exec -it -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubisrv | awk '{print $1}'` -- cat /opt/kyuubi/work/hdfs/kyuubi-spark-sql-engine.log.0
+
 
 :<<EOF
 NAME: my
@@ -209,28 +220,26 @@ Get kyuubi expose URL by running these commands:
   echo $NODE_IP:$NODE_PORT
 EOF
 
-kubectl exec -it -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubi | awk '{print $1}'` -- bash
+kubectl exec -it -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubisrv | awk '{print $1}'` -- bash
+kubectl exec -it -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubisrv | awk '{print $1}'` -- cat conf/kyuubi-defaults.conf
 
-kubectl exec -it -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubi | awk '{print $1}'` -- cat conf/kyuubi-defaults.conf
+kubectl get pod -n spark-operator |grep Running | grep kyuubi-connection-spark-sql | awk '{print $1}'| xargs kubectl delete pod "$1" -n spark-operator --force --grace-period=0
+kubectl get pod -n spark-operator |grep Running | grep exec | awk '{print $1}'| xargs kubectl delete pod "$1" -n spark-operator --force --grace-period=0
+kubectl get pod -n spark-operator |grep -v Running | grep kyuubi-connection-spark-sql | awk '{print $1}'| xargs kubectl delete pod "$1" -n spark-operator --force --grace-period=0
 
-kubectl exec -it -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep spark-test | awk '{print $1}'` -- bash
-  #beeline -u jdbc:hive2://kyuubi-thrift-binary.spark-operator.svc.cluster.local:10009 -n kyuubi
-  beeline -u jdbc:hive2://kyuubi-thrift-binary.spark-operator.svc.cluster.local:10009 -n hdfs
-    SHOW DATABASES;
-    USE tpcds_bin_partitioned_orc_10;
-    SHOW TABLES;
-    SELECT * FROM store_sales LIMIT 5;
+:<<EOF
+不设置kyuubi.engine.share.level=CONNECTION，缺省在USER模式，一个-n USER会复用1个driver Pod一直在Running状态
+设置了CONNECTION level后，driver从Running到Completed状态，executor pod会Terminated被删除
 
-
+如果不把kyuubi提交任务模式explicitly设置为cluster，会默认用client方式运行，会遇到和spark集成volcano测试时client模式一样的错误
+    spark.submit.deployMode=cluster
 23/03/09 06:55:17 WARN ExecutorPodsSnapshotsStoreImpl: Exception when notifying snapshot subscriber.
 io.fabric8.kubernetes.client.KubernetesClientException: Failure executing: POST at: https://kubernetes.default.svc.cluster.local/api/v1/namespaces/spark-operator/pods. Message: admission webhook "validatepod.volcano.sh" denied the request: failed to get PodGroup for pod <spark-operator/kyuubi-a3fcecd5-cbba-40b6-a5d3-bdfce7bfa87f-exec-4>: podgroups.scheduling.volcano.sh "spark-cf4135d392c14122add6a8f10fa1511e-podgroup" not found. Received status: Status(apiVersion=v1, code=400, details=null, kind=Status, message=admission webhook "validatepod.volcano.sh" denied the request: failed to get PodGroup for pod <spark-operator/kyuubi-a3fcecd5-cbba-40b6-a5d3-bdfce7bfa87f-exec-4>: podgroups.scheduling.volcano.sh "spark-cf4135d392c14122add6a8f10fa1511e-podgroup" not found, metadata=ListMeta(_continue=null, remainingItemCount=null, resourceVersion=null, selfLink=null, additionalProperties={}), reason=null, status=Failure, additionalProperties={}).
 23/01/30 10:12:51 WARN ExecutorPodsSnapshotsStoreImpl: Exception when notifying snapshot subscriber.
 io.fabric8.kubernetes.client.KubernetesClientException: Failure executing: POST at: https://kubernetes.default.svc.cluster.local/api/v1/namespaces/spark-operator/pods. Message: admission webhook "validatepod.volcano.sh" denied the request: failed to get PodGroup for pod <spark-operator/spark-sql-job-test-manual-10-q1-89a4df860229efaa-exec-3>: podgroups.scheduling.volcano.sh "spark-0f19d22bb4564761a869067c3c54eb51-podgroup" not found. Received status: Status(apiVersion=v1, code=400, details=null, kind=Status, message=admission webhook "validatepod.volcano.sh" denied the request: failed to get PodGroup for pod <spark-operator/spark-sql-job-test-manual-10-q1-89a4df860229efaa-exec-3>: podgroups.scheduling.volcano.sh "spark-0f19d22bb4564761a869067c3c54eb51-podgroup" not found, metadata=ListMeta(_continue=null, remainingItemCount=null, resourceVersion=null, selfLink=null, additionalProperties={}), reason=null, status=Failure, additionalProperties={}).
 
-    spark.submit.deployMode=cluster
-
+日志里有kyuubi组装的spark-submit语句，调试时优先看这个是否配置在kyuubi-defaults.conf的配置是否会转换出现在这里，提交时间被用来判断任务超时，调试时可以删掉
 	--conf spark.kyuubi.engine.submit.time=1678355305394 \
-
 /app/hdfs/spark/bin/spark-submit \
 	--class org.apache.kyuubi.engine.spark.SparkSQLEngine \
 	--conf spark.hive.server2.thrift.resultset.default.fetch.size=1000 \
@@ -255,3 +264,5 @@ io.fabric8.kubernetes.client.KubernetesClientException: Failure executing: POST 
 	--conf spark.kubernetes.driverEnv.SPARK_USER_NAME=hdfs \
 	--conf spark.executorEnv.SPARK_USER_NAME=hdfs \
 	--proxy-user hdfs /opt/kyuubi/externals/engines/spark/kyuubi-spark-sql-engine_2.12-1.7.0.jar
+
+EOF
---
+++ spark/test/kyuubi-test.sh
@@ -0,0 +1,217 @@
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Mac detected."
+    #mac
+    os=darwin
+    MYHOME=/Volumes/data
+    SED=gsed
+    bin=/Users/apple/bin
+else
+    echo "Assuming linux by default."
+    #linux
+    os=linux
+    MYHOME=~
+    SED=sed
+    bin=/usr/local/bin
+fi
+
+WORK_HOME=${MYHOME}/workspace
+PRJ_HOME=${WORK_HOME}/dockerfile
+SPARK_HOME=${PRJ_HOME}/spark
+
+SPARK_VERSION=3.3.1
+
+#KYUUBI_VERSION=1.6.1-incubating
+KYUUBI_VERSION=1.7.0
+KYUUBI_HOME=${SPARK_HOME}/apache-kyuubi-${KYUUBI_VERSION}-bin
+
+BASE_IMAGE=harbor.my.org:1080/bronzels/spark-juicefs-volcano-rss:${SPARK_VERSION}
+
+
+cd ${SPARK_HOME}/test
+
+kubectl get pod -n spark-operator |grep Running | grep kyuubi-connection-spark-sql | awk '{print $1}'| xargs kubectl delete pod "$1" -n spark-operator --force --grace-period=0
+kubectl get pod -n spark-operator |grep Running | grep exec | awk '{print $1}'| xargs kubectl delete pod "$1" -n spark-operator --force --grace-period=0
+kubectl get pod -n spark-operator |grep -v Running | grep kyuubi-connection-spark-sql | awk '{print $1}'| xargs kubectl delete pod "$1" -n spark-operator --force --grace-period=0
+
+kubectl exec -it -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep spark-test | awk '{print $1}'` -- bash
+  #beeline -u jdbc:hive2://kyuubi-thrift-binary.spark-operator.svc.cluster.local:10009 -n kyuubi
+  beeline -u jdbc:hive2://kyuubisrv-thrift-binary.spark-operator.svc.cluster.local:10009 -n hdfs
+  #大概有20s才能连接成功
+  #会有1个driver，1个executor pod在Running状态，过几秒以后exec-1会消失，以后如果有查询会从exec-2开始新创建
+kubectl get pod -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubi-connection-spark-sql | awk '{print $1}'` -o yaml
+    resources:
+      limits:
+        memory: 1408Mi
+      requests:
+        cpu: "1"
+        memory: 1408Mi
+    SHOW DATABASES;
+    USE tpcds_bin_partitioned_orc_10;
+    SHOW TABLES;
+    SELECT * FROM store_sales LIMIT 5;
+kubectl get pod -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubi | grep 'exec-2 ' | awk '{print $1}'` -o yaml
+    resources:
+      limits:
+        memory: 1408Mi
+      requests:
+        cpu: "1"
+        memory: 1408Mi
+:<<EOF
+对一个很大的表SELECT * LIMIT 5，启动30多个executor，查询结束后很多还在pending状态
+退出beeline时，就能自动删掉exector的pod，driver的pod也变成Completed状态。
+EOF
+    SELECT COUNT(1) FROM store_sales;
+    !quit
+    -- 或者ctrl + d退出
+:<<EOF
+执行了一个对很大的表的SELECT COUNT(1)，启动50多个executor，这个查询做完了，pending状态的executor会消失，只剩下running的，这些exec pod只要beeline不退出，就一直是Running状态。
+执行时间1分钟10s。
+这样如果是对接1个BI，譬如superset之类的，数据分析人员的查询窗口如果一直开着，是不是exec pod都一直保持在最大资源占用的查询跑起来的数目，就算他啥都不查，这些资源都还是占着。
+出现这种情况以后，sharelevel设置为CONNECTION，退出beeline时，driver/executor的pod都还是Running状态。
+EOF
+    SELECT COUNT(1) FROM call_center;
+:<<EOF
+执行了一个对很小的表的SELECT COUNT(1)，executor的数目还是1。
+退出beeline时，就能自动删掉exector的pod，driver的pod也变成Completed状态。
+EOF
+
+  #beeline -n hdfs -u 'jdbc:hive2://kyuubisrv-thrift-binary.spark-operator.svc.cluster.local:10009/tpcds_bin_partitioned_orc_10;spark.kubernetes.scheduler.volcano.podGroupTemplateFile=/app/hdfs/spark/work-dir/podgroups/volcano-halfavailable-podgroup-high.yaml;spark.driver.memory=2g;spark.driver.cores=1;spark.executor.memory=4g;spark.executor.cores=1；'
+  #beeline -n hdfs -u 'jdbc:hive2://kyuubisrv-thrift-binary.spark-operator.svc.cluster.local:10009/tpcds_bin_partitioned_orc_10;kyuubi.engineEnv.spark.kubernetes.scheduler.volcano.podGroupTemplateFile=/app/hdfs/spark/work-dir/podgroups/volcano-halfavailable-podgroup-high.yaml;kyuubi.engineEnv.spark.driver.memory=2g;kyuubi.engineEnv.spark.driver.cores=1;kyuubi.engineEnv.spark.executor.memory=4g;kyuubi.engineEnv.spark.executor.cores=1；'
+  #数据库名到第一个配置项中间是;#，漏了#不行
+  beeline -n hdfs -u 'jdbc:hive2://kyuubisrv-thrift-binary.spark-operator.svc.cluster.local:10009/tpcds_bin_partitioned_orc_10;#spark.kubernetes.scheduler.volcano.podGroupTemplateFile=/app/hdfs/spark/work-dir/podgroups/volcano-halfavailable-podgroup-high.yaml;spark.driver.memory=2g;spark.driver.cores=1;spark.executor.memory=4g;spark.executor.cores=1;'
+  #从server的日志检查检查连接设置正确格式化成了spark-submit命令
+kubectl logs -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubisrv | awk '{print $1}'`
+/app/hdfs/spark/bin/spark-submit \
+	--class org.apache.kyuubi.engine.spark.SparkSQLEngine \
+	--conf spark.hive.server2.thrift.resultset.default.fetch.size=1000 \
+	--conf spark.kyuubi.client.ipAddress=100.110.242.100 \
+	--conf spark.kyuubi.engine.share.level=CONNECTION \
+	--conf spark.kyuubi.engine.submit.time=1678420345460 \
+	--conf spark.kyuubi.frontend.protocols=THRIFT_BINARY \
+	--conf spark.kyuubi.ha.engine.ref.id=9e43bdd8-5057-4e53-a9ff-769d720fbf99 \
+	--conf spark.kyuubi.ha.namespace=/kyuubi_1.7.0_CONNECTION_SPARK_SQL/hdfs/9e43bdd8-5057-4e53-a9ff-769d720fbf99 \
+	--conf spark.kyuubi.ha.zookeeper.quorum=zk-zookeeper \
+	--conf spark.kyuubi.kubernetes.namespace=spark-operator \
+	--conf spark.kyuubi.server.ipAddress=0.0.0.0 \
+	--conf spark.kyuubi.session.connection.url=0.0.0.0:10009 \
+	--conf spark.kyuubi.session.real.user=hdfs \
+	--conf spark.app.name=kyuubi_CONNECTION_SPARK_SQL_hdfs_9e43bdd8-5057-4e53-a9ff-769d720fbf99 \
+
+	--conf spark.driver.cores=1 \
+	--conf spark.driver.memory=2g \
+	--conf spark.executor.cores=1 \
+	--conf spark.executor.memory=4g \
+
+	--conf spark.kubernetes.container.image=harbor.my.org:1080/bronzels/spark-juicefs-volcano-rss:3.3.1 \
+	--conf spark.kubernetes.driver.label.kyuubi-unique-tag=9e43bdd8-5057-4e53-a9ff-769d720fbf99 \
+	--conf spark.kubernetes.driver.pod.featureSteps=org.apache.spark.deploy.k8s.features.VolcanoFeatureStep \
+	--conf spark.kubernetes.executor.pod.featureSteps=org.apache.spark.deploy.k8s.features.VolcanoFeatureStep \
+	--conf spark.kubernetes.scheduler.name=volcano \
+
+	--conf spark.kubernetes.scheduler.volcano.podGroupTemplateFile=/app/hdfs/spark/work-dir/podgroups/volcano-halfavailable-podgroup-high.yaml \
+
+	--conf spark.submit.deployMode=cluster \
+	--conf spark.kubernetes.driverEnv.SPARK_USER_NAME=hdfs \
+	--conf spark.executorEnv.SPARK_USER_NAME=hdfs \
+	--proxy-user hdfs /opt/kyuubi/externals/engines/spark/kyuubi-spark-sql-engine_2.12-1.7.0.jar
+  #driver的日志里有driver/executor设置不生效的warning提示
+23/03/10 03:52:44 WARN SparkSessionImpl:  Cannot modify the value of a Spark config: spark.driver.memory. See also 'https://spark.apache.org/docs/latest/sql-migration-guide.html#ddl-statements'
+23/03/10 03:52:44 WARN SparkSessionImpl:  Cannot modify the value of a Spark config: spark.driver.cores. See also 'https://spark.apache.org/docs/latest/sql-migration-guide.html#ddl-statements'
+23/03/10 03:52:44 WARN SparkSessionImpl:  Cannot modify the value of a Spark config: spark.executor.memory. See also 'https://spark.apache.org/docs/latest/sql-migration-guide.html#ddl-statements'
+23/03/10 03:52:44 WARN SparkSessionImpl:  Cannot modify the value of a Spark config: spark.executor.cores. See also 'https://spark.apache.org/docs/latest/sql-migration-guide.html#ddl-statements'
+  #从pod的yaml检查driver/executor设置生效了
+kubectl get pod -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubi-connection-spark-sql | awk '{print $1}'` -o yaml
+    resources:
+      limits:
+        memory: 2432Mi
+      requests:
+        cpu: "1"
+        memory: 2432Mi
+    SELECT * FROM store_sales LIMIT 5;
+kubectl get pod -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubi | grep 'exec-2 ' | awk '{print $1}'` -o yaml
+    resources:
+      limits:
+        memory: 4505Mi
+      requests:
+        cpu: "1"
+        memory: 4505Mi
+
+
+cat << EOF > setting.sql
+SET spark.kubernetes.scheduler.volcano.podGroupTemplateFile=/app/hdfs/spark/work-dir/podgroups/volcano-halfavailable-podgroup-high.yaml;
+SET spark.driver.memory=2g;
+SET spark.driver.cores=1;
+SET spark.executor.memory=4g;
+SET spark.executor.cores=1;
+EOF
+echo "SELECT * FROM store_sales LIMIT 5" > query.sql
+beeline -n hdfs -u 'jdbc:hive2://kyuubisrv-thrift-binary.spark-operator.svc.cluster.local:10009/tpcds_bin_partitioned_orc_10' -i setting.sql -f query.sql
+  #从server的日志检查检查连接设置格式化成了spark-submit命令，没有包括-i的设置内容
+kubectl logs -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubisrv | awk '{print $1}'`
+/app/hdfs/spark/bin/spark-submit \
+	--class org.apache.kyuubi.engine.spark.SparkSQLEngine \
+	--conf spark.hive.server2.thrift.resultset.default.fetch.size=1000 \
+	--conf spark.kyuubi.client.ipAddress=100.110.242.100 \
+	--conf spark.kyuubi.engine.share.level=CONNECTION \
+	--conf spark.kyuubi.engine.submit.time=1678421325605 \
+	--conf spark.kyuubi.frontend.protocols=THRIFT_BINARY \
+	--conf spark.kyuubi.ha.engine.ref.id=16320969-fa53-40a8-88b7-91b3ae79ea41 \
+	--conf spark.kyuubi.ha.namespace=/kyuubi_1.7.0_CONNECTION_SPARK_SQL/hdfs/16320969-fa53-40a8-88b7-91b3ae79ea41 \
+	--conf spark.kyuubi.ha.zookeeper.quorum=zk-zookeeper \
+	--conf spark.kyuubi.kubernetes.namespace=spark-operator \
+	--conf spark.kyuubi.server.ipAddress=0.0.0.0 \
+	--conf spark.kyuubi.session.connection.url=0.0.0.0:10009 \
+	--conf spark.kyuubi.session.real.user=hdfs \
+	--conf spark.app.name=kyuubi_CONNECTION_SPARK_SQL_hdfs_16320969-fa53-40a8-88b7-91b3ae79ea41 \
+	--conf spark.kubernetes.container.image=harbor.my.org:1080/bronzels/spark-juicefs-volcano-rss:3.3.1 \
+	--conf spark.kubernetes.driver.label.kyuubi-unique-tag=16320969-fa53-40a8-88b7-91b3ae79ea41 \
+	--conf spark.kubernetes.driver.pod.featureSteps=org.apache.spark.deploy.k8s.features.VolcanoFeatureStep \
+	--conf spark.kubernetes.executor.pod.featureSteps=org.apache.spark.deploy.k8s.features.VolcanoFeatureStep \
+	--conf spark.kubernetes.scheduler.name=volcano \
+	--conf spark.kubernetes.scheduler.volcano.podGroupTemplateFile=/app/hdfs/spark/work-dir/podgroups/volcano-halfavailable-podgroup.yaml \
+	--conf spark.submit.deployMode=cluster \
+	--conf spark.kubernetes.driverEnv.SPARK_USER_NAME=hdfs \
+	--conf spark.executorEnv.SPARK_USER_NAME=hdfs \
+	--proxy-user hdfs /opt/kyuubi/externals/engines/spark/kyuubi-spark-sql-engine_2.12-1.7.0.jar
+kubectl logs -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubi-connection-spark-sql | awk '{print $1}'`
+  #driver的日志里有driver/executor设置不生效的error提示
+23/03/10 04:09:05 ERROR ExecuteStatement: Error operating ExecuteStatement: org.apache.spark.sql.AnalysisException:  Cannot modify the value of a Spark config: spark.driver.memory. See also 'https://spark.apache.org/docs/latest/sql-migration-guide.html#ddl-statements'
+  #从pod的yaml检查driver/executor设置没有生效
+kubectl get pod -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubi-connection-spark-sql | awk '{print $1}'` -o yaml
+      resources:
+        requests:
+          cpu: 250m
+          memory: 256Mi
+kubectl get pod -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubi | grep 'exec-2 ' | awk '{print $1}'` -o yaml
+      resources:
+        requests:
+          cpu: 250m
+          memory: 256Mi
+
+
+USER=hdfs
+cat << EOF > setting.sql
+SET ___${USER}___.spark.kubernetes.scheduler.volcano.podGroupTemplateFile=/app/hdfs/spark/work-dir/podgroups/volcano-halfavailable-podgroup-high.yaml;
+SET ___${USER}___.spark.driver.memory=2g;
+SET ___${USER}___.spark.driver.cores=1;
+SET ___${USER}___.spark.executor.memory=4g;
+SET ___${USER}___.spark.executor.cores=1;
+EOF
+echo "SELECT * FROM store_sales LIMIT 5" > query.sql
+beeline -n ${USER} -u 'jdbc:hive2://kyuubisrv-thrift-binary.spark-operator.svc.cluster.local:10009/tpcds_bin_partitioned_orc_10' -i setting.sql -f query.sql
+  #driver的日志里有相关提示，但是似乎没有生效
+:<<EOF
+23/03/10 04:22:40 INFO ExecuteStatement: Processing hdfs's query[3ee89efd-4b89-4567-9e45-fe27dbb89f72]: PENDING_STATE -> RUNNING_STATE, statement:
+SET ___hdfs___.spark.driver.memory=2g
+EOF
+  #从pod的yaml检查driver/executor设置没有生效
+kubectl get pod -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubi-connection-spark-sql | awk '{print $1}'` -o yaml
+      resources:
+        requests:
+          cpu: 250m
+          memory: 256Mi
+kubectl get pod -n spark-operator `kubectl get pod -n spark-operator | grep Running | grep kyuubi | grep 'exec-2 ' | awk '{print $1}'` -o yaml
+      resources:
+        requests:
+          cpu: 250m
+          memory: 256Mi
